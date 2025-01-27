44736cfa3718c27f868703e0a82b1b8708cc6d0f
Isaías Roberto <76740216+anidio@users.noreply.github.com>
2025-01-27 00:37:00 -0300

    incluindo dedsafio 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,70 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        String nomeNinja1 = "Rocklee";
+        int idadeNinja1 = 12;
+        String missao1 = "Avançar rank paase tornar ninja realmente";
+        String statusDaMissao = "Em andamento";
+        char nivel1DaMissao = 'A';
+
+        if(idadeNinja1 < 15){
+            if(nivel1DaMissao == 'C' || nivel1DaMissao == 'D'){
+                statusDaMissao = "Concluida";
+            }else{
+                statusDaMissao = "Não concluida, você não tem idade suficiente";
+            }
+        }else{
+            statusDaMissao = "Concluida";
+        }
+        System.out.println("------------------------------------------------");
+        System.out.println("Nome do Ninja: " + nomeNinja1);
+        System.out.println("Idade do Ninja: " + idadeNinja1);
+        System.out.println("Missao: " + missao1);
+        System.out.println("Nivel da Missao " + nivel1DaMissao);
+        System.out.println("Status da missao: " + statusDaMissao);
+        System.out.println("------------------------------------------------");
+
+
+        String nomeNinja2 = "Jaxon";
+        int idadeNinja2 = 15;
+        String missao2 = "Caçar  reconpensas para  se tornar Jounin";
+        char nivel3DaMissao = 'C';
+
+        if(idadeNinja2 < 15){
+            if(nivel1DaMissao == 'C' || nivel1DaMissao == 'D'){
+                statusDaMissao = "Concluida";
+            }else{
+                statusDaMissao = "Não concluida, você não tem idade suficiente";
+            }
+        }else{
+            statusDaMissao = "Concluida";
+        }
+        System.out.println("Nome do Ninja: " + nomeNinja2);
+        System.out.println("Idade do Ninja: " + idadeNinja2);
+        System.out.println("Missao: " + missao2);
+        System.out.println("Nivel da Missao " + nivel1DaMissao);
+        System.out.println("Status da missao: " + statusDaMissao);
+        System.out.println("------------------------------------------------");
+
+        String  nomeNinja3 ="Sasuke";
+        int idadeNinja3 = 16;
+        String missao3 = "Defender aldeias menores do artaque de ninjas do Mal";
+        char nivel4DaMissao = 'D';
+
+        if(idadeNinja3 < 15){
+            if(nivel1DaMissao == 'C' || nivel1DaMissao == 'D'){
+                statusDaMissao = "Concluida";
+            }else{
+                statusDaMissao = "Não concluida, você não tem idade suficiente";
+            }
+        }else{
+            statusDaMissao = "Concluida";
+        }
+        System.out.println("Nome do Ninja: " + nomeNinja3);
+        System.out.println("Idade do Ninja: " + idadeNinja3);
+        System.out.println("Missao: " + missao3);
+        System.out.println("Nivel da Missao " + nivel1DaMissao);
+        System.out.println("Status da missao: " + statusDaMissao);
+        System.out.println("------------------------------------------------");
     }
 }
--- src/main/java/org/example/desafios1/Ninjas.java
+++ src/main/java/org/example/desafios1/Ninjas.java
@@ -1,18 +1,7 @@
 package org.example.desafios1;
 
+import java.io.StringReader;
+
 public class Ninjas {
-
-    String nomeNinja1 = "Rocklee";
-    int idaideNinja1 = 12;
-
-    String nomeNinja2 = "Jaxon";
-    int ninja2 = 15;
-
-    String  nomeNinja3 ="Sasuke";
-    int ninja3 = 14;
-
-    String missao1 = "Avançar rank paase tornar ninja realmente";
-    String missao2 = "Caçar  reconpensas para  se tornar Jounin";
-    String missao3 = "Defender aldeias menores do artaque de ninjas do Mal";
 
 }
