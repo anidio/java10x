bb8fc57a5ddba365faf5c0ab25ef150dfe849fa1
Isaías Roberto <76740216+anidio@users.noreply.github.com>
2025-01-26 23:52:52 -0300

    first commit

---
+++ src/main/java/org/example/desafios1/Ninjas.java
@@ -0,0 +1,18 @@
+package org.example.desafios1;
+
+public class Ninjas {
+
+    String nomeNinja1 = "Rocklee";
+    int idaideNinja1 = 12;
+
+    String nomeNinja2 = "Jaxon";
+    int ninja2 = 15;
+
+    String  nomeNinja3 ="Sasuke";
+    int ninja3 = 14;
+
+    String missao1 = "Avançar rank paase tornar ninja realmente";
+    String missao2 = "Caçar  reconpensas para  se tornar Jounin";
+    String missao3 = "Defender aldeias menores do artaque de ninjas do Mal";
+
+}
