9787d945937bc434df593646ac58d2af15d32978
Isaías Roberto <76740216+anidio@users.noreply.github.com>
2025-01-28 01:16:11 -0300

    scannerr e validaççção de dados

---
+++ src/main/java/org/example/condicoes/ScannerDeUsuaio.java
@@ -0,0 +1,26 @@
+package org.example.condicoes;
+
+import java.util.Scanner;
+
+public class ScannerDeUsuaio {
+
+    public static void main(String[] args) {
+        Scanner caixaDeTexto = new Scanner(System.in);
+
+        //recebe nome do ninja
+        System.out.println("Esceva aqui o nome do ninja: ");
+        String nomeDoNinja = caixaDeTexto.nextLine();
+
+        //recebe idade do ninja
+        System.out.println("Esceva aqui a idade do seu ninja: ");
+        int  idadeDoNinja = caixaDeTexto.nextInt();
+        System.out.println("O nome do seu ninja é: " + nomeDoNinja + ", e sua idade é: " + idadeDoNinja + "anos");
+
+        if (idadeDoNinja >= 18){
+            System.out.println("Esse ninja já é maior de idade e pode ir para a missão fora da aldeia");
+        }else{
+            System.out.println("Esse ninja é muito novo, ainda precisa treinar mais" );
+        }
+        caixaDeTexto.close();
+    }
+}
