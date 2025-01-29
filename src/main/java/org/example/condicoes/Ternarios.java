a002fbb7d819f9a81c87e910789870cd779c845b
Isaías Roberto <76740216+anidio@users.noreply.github.com>
2025-01-29 00:23:08 -0300

    ternarios

--- src/main/java/org/example/condicoes/Ternarios.java
+++ src/main/java/org/example/condicoes/Ternarios.java
@@ -3,5 +3,8 @@ package org.example.condicoes;
 public class Ternarios {
     public static void main(String[] args) {
 
+        short numeroDeMissoes = 11;
+        String nivelDoNinja = (numeroDeMissoes) >= 10 ? "Esse ninja esta com mais de 10 missoes" : "Esse ninja esta com menos de 10 missoes";
+        System.out.println(nivelDoNinja);
     }
 }
