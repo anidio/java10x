29bd5d91d9d49d64c0459ddf9257c51374404be6
Isaías Roberto <76740216+anidio@users.noreply.github.com>
2025-01-31 22:44:15 -0300

    array

---
+++ src/main/java/org/example/NivelBasico/Array.java
@@ -0,0 +1,15 @@
+package org.example.NivelBasico;
+
+public class Array {
+    public static void main(String[] args) {
+
+        String[] ninja = new String[4];
+        ninja[0] = "Rocklee";
+        ninja[1] = "Sakura Haruno";
+        ninja[2] = "Sasukke Uchiha";
+        ninja[3] = "Naruto Uzumaki";
+        ninja[3] = "Hinata  Hyuga";
+
+        System.out.println(ninja[1]);
+    }
+}
---
+++ src/main/java/org/example/condicoes/LacosDeRepeticao.java
@@ -0,0 +1,21 @@
+package org.example.condicoes;
+
+public class LacosDeRepeticao {
+
+    public static void main(String[] args) {
+
+        // While
+        int numeroDeClones = 0;
+        int numeroMaximoDeClones = 40;
+
+//        while (numeroDeClones <= numeroMaximoDeClones){
+//            System.out.println("O Naruto fez " + numeroDeClones+ " clones das sombras");
+//            numeroDeClones++;
+//        }
+
+        // FOR
+        for (int i = 0; i <= numeroMaximoDeClones ; i++) {
+            System.out.println("O Naruto fez " + i + " clones das sombras");
+        }
+    }
+}
